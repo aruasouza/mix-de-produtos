689b42c53393afe8e4d7e4939cdf3e36458e6145
aruasouza <arua.viso@gmail.com>
2023-11-30 15:39:15 -0300

    edit table

--- dash.py
+++ dash.py
@@ -3,31 +3,28 @@ import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
 
-mod_color = {'Manter':'yellow','Remover':'red','Incluir':'green'}
+mod_color = {'Manter':'yellow','Remover':'red','Incluir':'green','flag':'Flag'}
+ren = {'loja':'Loja','secao':'Seção','produto':'Produto','mod':'Ação','flag':'Flag'}
+unren = {ren[key]:key for key in ren}
 def load_data():
-    df = pd.read_csv('modificacoes_mix.csv',sep = ';')
-    df['secao'] = df['secao'].apply(lambda x: x.title())
+    df = pd.read_csv('modificacoes_mix.csv',sep = ';').rename(ren,axis = 1)
+    df['Seção'] = df['Seção'].apply(lambda x: x.title())
     return df
 
 def refilter():
+    apply_changes()
     data = st.session_state.data
     loja = st.session_state.current_loja
-    st.session_state.filtered_data = data.loc[data['loja'] == loja]
-
-def flag():
-    row = st.session_state.row
-    st.session_state.data.loc[row,'flag'] = not st.session_state.data.loc[row,'flag']
-    refilter()
-    st.sidebar.info(f'Flag da linha {st.session_state.row} modificada')
-
-def remove_all():
-    st.session_state.data['flag'] = False
-    refilter()
-    st.sidebar.info('Todas as flags foram removidas')
+    st.session_state.filtered_data = data.loc[data['Loja'] == loja]
 
 def save_changes():
-    st.session_state.data.to_csv('modificacoes_mix.csv',index = False,sep = ';')
+    apply_changes()
+    st.session_state.data.rename(unren,axis = 1).to_csv('modificacoes_mix.csv',index = False,sep = ';')
     st.sidebar.success('Alterações salvas')
+
+def apply_changes():
+    changes = st.session_state.changes['Flag']
+    st.session_state.data.loc[changes.index,'Flag'] = changes
 
 html = '''
 <style>
@@ -48,47 +45,44 @@ if 'not_first_load' not in st.session_state:
     data = load_data()
     st.session_state.not_first_load = True
     st.session_state.data = data
-    st.session_state.lojas = list(data['loja'].unique())
-    st.session_state.secoes = list(data['secao'].unique())
-    st.session_state.filtered_data = data.loc[data['loja'] == st.session_state.lojas[0]]
-    st.session_state.prod_map = data[['produto','secao']].drop_duplicates().set_index('produto')['secao']
+    st.session_state.lojas = list(data['Loja'].unique())
+    st.session_state.secoes = list(data['Seção'].unique())
+    st.session_state.filtered_data = data.loc[data['Loja'] == st.session_state.lojas[0]]
+    st.session_state.prod_map = data[['Produto','Seção']].drop_duplicates().set_index('Produto')['Seção']
 
 with st.sidebar:
-    st.toggle('Visualizar gráfico',key = 'toggle')
     st.selectbox('Loja',st.session_state.lojas,key = 'current_loja',on_change = refilter)
-    st.header('Inserir ou remover flag')
-    st.number_input('Linha',min_value = 0,max_value = st.session_state.data.index[-1],value = 0,key = 'row')
-    c1,c2 = st.columns(2)
-    c1.button('Inserir/remover',on_click = flag)
-    c2.button('Limpar flags',on_click = remove_all)
+    st.radio('Menu',['Tabela','Gráfico','Flags'],key = 'menu',on_change = refilter)
     st.button('Salvar mudanças',on_click = save_changes)
 
-if not st.session_state.toggle:
-    ren = {'loja':'Loja','secao':'Seção','produto':'Produto','mod':'Ação'}
+h = 500
+if st.session_state.menu == 'Tabela':
     df = st.session_state.filtered_data
     c1,c2 = st.columns(2)
-    c1.selectbox('Seção',['Todas'] + st.session_state.secoes,key = 'current_secao')
-    c2.selectbox('Ação',['Todas','Manter/Incluir','Manter','Incluir','Remover'],index = 1,key = 'acao')
+    c1.selectbox('Seção',['Todas'] + st.session_state.secoes,key = 'current_secao',on_change = refilter)
+    c2.selectbox('Ação',['Todas','Manter/Incluir','Manter','Incluir','Remover'],index = 1,key = 'acao',on_change = refilter)
     secao = st.session_state.current_secao
     acao = st.session_state.acao
-    h = 500
     if secao != 'Todas':
-        filt = df['secao'] == secao
+        filt = df['Seção'] == secao
     else:
         filt = pd.Series([True] * len(df),df.index)
     if acao == 'Manter/Incluir':
-        filt = filt & ((df['mod'] == 'Manter') | (df['mod'] == 'Incluir'))
+        filt = filt & ((df['Ação'] == 'Manter') | (df['Ação'] == 'Incluir'))
     elif acao != 'Todas':
-        filt = filt & (df['mod'] == acao)
-    st.dataframe(df.loc[filt].rename(ren,axis = 1)\
-                    .style.apply(lambda row: ['background-color: yellow' if row['flag'] else ''] * len(row),axis = 1),
-                    use_container_width = True,height = h,column_config = {'flag':None})
+        filt = filt & (df['Ação'] == acao)
+    st.session_state.changes = st.data_editor(df.loc[filt],hide_index = True,
+                    use_container_width = True,height = h,disabled = ('Loja','Seção','Produto','Ação'))
+elif st.session_state.menu == 'Flags':
+    df = st.session_state.data
+    st.session_state.changes = st.data_editor(df.loc[df['Flag']],hide_index = True,
+                    use_container_width = True,height = h,disabled = ('Loja','Seção','Produto','Ação'))
 else:
     df = st.session_state.filtered_data
     loja = st.session_state.current_loja
     prod_map = st.session_state.prod_map
-    mod_map = df[['produto','mod']].drop_duplicates().set_index('produto')['mod']
-    prods,secs = list(df['produto'].unique()),list(df['secao'].unique())
+    mod_map = df[['Produto','Ação']].drop_duplicates().set_index('Produto')['Ação']
+    prods,secs = list(df['Produto'].unique()),list(df['Seção'].unique())
     labels = prods + secs + [loja]
     parents = [prod_map[p] for p in prods] + [loja] * len(secs) + ['']
     colors = [mod_color[mod_map[p]] for p in prods] + ['orange'] * len(secs) + ['white']
